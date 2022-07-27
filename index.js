fa6f971f0d99a50946926de0b344087fe1c4d599
LouisCorin <iordanescucorinlouis@gmail.com>
2022-07-27 19:28:19 +0300

    Build js

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+let password1 = document.getElementById("password-1");
+let password2 = document.getElementById("password-2");
+let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function genPassword() {
+    let passwordLength = 15;
+    let password1 = "";
+    let password2 = "";
+    for (let i = 0; i < passwordLength; i++) {
+        let randomNumber = Math.floor(Math.random() * chars.length);
+        let randomNumber2 = Math.floor(Math.random() * chars.length);
+        password1 += chars[randomNumber]
+        password2 += chars[randomNumber2]
+    }
+    document.getElementById("password-1").value = password1;
+    document.getElementById("password-2").value = password2;
+
+}
